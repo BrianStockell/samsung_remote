778b6abf67391fab832414ae07b8c5e6ccafba2a
Brian Stockell <brian@rcmdevops.onmicrosoft.com>
2022-08-21 08:53:16 -0500

    create menu command

---
+++ macros_samples/KEY_MENU.m
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+import samsungctl
+import time
+
+config = {
+    "name": "samsungctl",
+    "description": "PC",
+    "id": "",
+    "host": "10.41.77.202",
+    "port": 55000,
+    "method": "websocket",
+    "timeout": 0,
+}
+
+#with samsungctl.Remote(config) as remote:
+#    for i in range(1):
+#        remote.control("KEY_CONTENTS")
+#        time.sleep(0.5)
+
+# with samsungctl.Remote(config) as remote:
+#    for i in range(1):
+#        remote.control("KEY_POWEROFF")
+#        time.sleep(0.5)
+
+with samsungctl.Remote(config) as remote:
+    for i in range(2):
+        remote.control("KEY_SOURCE")
+        time.sleep(0.5)
